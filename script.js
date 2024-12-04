f16cf6d5c75d336767614ba3303b708f30e47229
Edwin <77319863+Edwin-KT@users.noreply.github.com>
2024-12-04 17:49:49 +0200

    add canvas size button functionality + refac code

--- script.js
+++ script.js
@@ -1,20 +1,45 @@
-let isDrawing = false;
+const canvas = document.querySelector(".canvas-container");
+const clearButton = document.querySelector(".clear-button");
+const size16Button = document.querySelector(".size16-button");
+const size32Button = document.querySelector(".size32-button");
+const size64Button = document.querySelector(".size64-button");
+let canvasSize = 16;
 
+clearButton.addEventListener("click", () => createCanvas(canvasSize));
+size16Button.addEventListener("click", () => {
+  canvasSize = 16;
+  createCanvas(canvasSize);
+});
+
+size32Button.addEventListener("click", () => {
+  canvasSize = 32;
+  createCanvas(canvasSize);
+});
+
+size64Button.addEventListener("click", () => {
+  canvasSize = 64;
+  createCanvas(canvasSize);
+});
+
+// drawing logic start
+let isDrawing = false;
 document.addEventListener("mousedown", (event) => {
   event.preventDefault();
   isDrawing = true;
 });
 document.addEventListener("mouseup", () => (isDrawing = false));
+// drawing logic end
 
-const canvas = document.querySelector(".canvas-container");
-function createCanvas(canvasSize) {
-  for (let i = 0; i < canvasSize; i++) {
+// canvas functions start
+function createCanvas(size) {
+  canvas.innerHTML = "";
+  for (let i = 0; i < size; i++) {
     const line = document.createElement("div");
     line.classList.add("canvas-line");
-    for (let j = 0; j < canvasSize; j++) {
+    for (let j = 0; j < size; j++) {
       const square = document.createElement("div");
       square.classList.add("square");
-      let squareSide = 512 / canvasSize;
+      let squareSide = 512 / size;
 
       square.style.width = `${squareSide}px`;
       square.style.height = `${squareSide}px`;
@@ -31,13 +56,6 @@ function createCanvas(canvasSize) {
   }
 }
 
-function clearCanvas(canvasSize) {
-  canvas.innerHTML = "";
-  createCanvas(canvasSize);
-}
+// canvas functions end
 
-createCanvas(16);
-
-const clearButton = document.querySelector(".clear-button");
-
-clearButton.addEventListener("click", () => clearCanvas(16));
+createCanvas(canvasSize);
